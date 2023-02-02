2ebb2cf24afeda41d44f8e6b6fa4ee5867c320ba
sbiljeek <sbiljeekdev@gmail.com>
2023-02-02 14:55:19 +0300

    Initial Commit

---
+++ CharacterIsEmoji/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CharacterIsEmoji
+//
+//  Created by Salman Biljeek on 2/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
